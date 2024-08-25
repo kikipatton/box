44eb76686d4b1e2e7619f400f757e0d0c241adfe
kiki patton <kiki@neesites.co.ke>
2024-08-25 17:48:36 +0000

    resources

--- client/views.py
+++ client/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+
+class ListView(TemplateView):
+    # Example of a function-based view
+    template_name = "client/clientlist.html"

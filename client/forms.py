fd9d42ce4ed2678824beb1000c6c38e562e8278a
kiki patton <kiki@neesites.co.ke>
2024-10-05 11:46:21 +0000

    ability add client done

--- client/forms.py
+++ client/forms.py
@@ -13,4 +13,5 @@ class ClientForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
         self.fields['phone_number'].required = True

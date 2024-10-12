793ee5900c295b26e041049e4bd49b0a55d3219b
kiki patton <kiki@neesites.co.ke>
2024-10-12 14:13:57 +0000

    more

--- box/settings.py
+++ box/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'box.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'tariff', 'templates')], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
